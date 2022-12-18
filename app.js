e373f8d3b1918ee7e5827ba23a4cc7d241b75c6e
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2022-12-18 17:12:11 +0530

    version 8

--- app.js
+++ app.js
@@ -69,3 +69,11 @@ app.listen(PORT,function(){
     console.log("server is up and running at 3000");
 })
 
+      // {
+      //   "src": "/style.css",
+      //   "dest": "/css/style.css"
+      // },
+      // {
+      //   "src": "/logo.png",
+      //   "dest": "/images/logo.png"
+      // }
